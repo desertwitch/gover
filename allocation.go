79359873d9262e232fbc0e686b065ce6cadaf382
desertwitch <24509509+desertwitch@users.noreply.github.com>
2025-02-12 09:58:02 +0100

    structure

--- allocation.go
+++ allocation.go
@@ -7,68 +7,6 @@ import (
 	"strings"
 )
 
-func getMoveables(source UnraidStoreable, share *UnraidShare) ([]*Moveable, error) {
-	var moveables []*Moveable
-
-	shareDir := filepath.Join(source.GetFSPath(), share.Name)
-	inodes := make(map[uint64]*Moveable)
-
-	err := filepath.WalkDir(shareDir, func(path string, d os.DirEntry, err error) error {
-		if err != nil {
-			fmt.Println("Error accessing path:", path, err)
-			return nil
-		}
-
-		isEmptyDir := false
-		if d.IsDir() {
-			isEmptyDir, err = isEmptyFolder(path)
-			if err != nil {
-				fmt.Println("Error checking for folder emptiness:", path, err)
-				return nil
-			}
-		}
-
-		if !d.IsDir() || (d.IsDir() && isEmptyDir) {
-			moveable := &Moveable{
-				Share:  share,
-				Path:   path,
-				Source: source,
-			}
-
-			moveables = append(moveables, moveable)
-		}
-
-		return nil
-	})
-	if err != nil {
-		return nil, fmt.Errorf("error walking directory: %w", err)
-	}
-
-	for _, m := range moveables {
-		metadata, err := getMetadata(m.Path)
-		if err != nil {
-			return nil, fmt.Errorf("failed to get metadata for %s: %v", m.Path, err)
-		}
-		m.Metadata = metadata
-
-		// TO-DO SYMLINKS
-		if value, exists := inodes[metadata.Inode]; exists {
-			m.Hardlink = true
-			m.HardlinkTo = value
-		} else {
-			inodes[metadata.Inode] = m
-		}
-
-		parents, err := walkParentDirs(m, shareDir)
-		if err != nil {
-			return nil, fmt.Errorf("failed to get parents for %s: %v", m.Path, err)
-		}
-		m.ParentDirs = parents
-	}
-
-	return moveables, nil
-}
-
 // func proposeDestinations(moveables []*Moveable, unraid *UnraidSystem) error {
 
 // }
