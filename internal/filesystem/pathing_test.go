dd5e65c0853398024120f11bbd6bd2a2ee679df0
desertwitch <24509509+desertwitch@users.noreply.github.com>
2025-03-01 16:59:56 +0100

    changes

--- internal/filesystem/pathing_test.go
+++ internal/filesystem/pathing_test.go
@@ -91,9 +91,9 @@ func TestEstablishPaths_FileNotExits(t *testing.T) {
 
 	assert.NoError(t, err)
 	assert.Len(t, filtered, 1)
-	assert.EqualValues(t, filtered[0], mockMoveable)
-	assert.EqualValues(t, filtered[0].DestPath, "/mnt/disk4/movies/file.mp4")
-	assert.EqualValues(t, filtered[0].RootDir.DestPath, "/mnt/disk4/movies")
+	assert.EqualValues(t, mockMoveable, filtered[0])
+	assert.EqualValues(t, "/mnt/disk4/movies/file.mp4", filtered[0].DestPath)
+	assert.EqualValues(t, "/mnt/disk4/movies", filtered[0].RootDir.DestPath)
 
 	mockOS.AssertExpectations(t)
 }
@@ -136,9 +136,9 @@ func TestEstablishPaths_TrailingSlashFile(t *testing.T) {
 
 	assert.NoError(t, err)
 	assert.Len(t, filtered, 1)
-	assert.EqualValues(t, filtered[0], mockMoveable)
-	assert.EqualValues(t, filtered[0].DestPath, "/mnt/disk4/movies/file.mp4")
-	assert.EqualValues(t, filtered[0].RootDir.DestPath, "/mnt/disk4/movies")
+	assert.EqualValues(t, mockMoveable, filtered[0])
+	assert.EqualValues(t, "/mnt/disk4/movies/file.mp4", filtered[0].DestPath)
+	assert.EqualValues(t, "/mnt/disk4/movies", filtered[0].RootDir.DestPath)
 
 	mockOS.AssertExpectations(t)
 }
@@ -181,9 +181,9 @@ func TestEstablishPaths_TrailingSlashDir(t *testing.T) {
 
 	assert.NoError(t, err)
 	assert.Len(t, filtered, 1)
-	assert.EqualValues(t, filtered[0], mockMoveable)
-	assert.EqualValues(t, filtered[0].DestPath, "/mnt/disk4/movies/dir")
-	assert.EqualValues(t, filtered[0].RootDir.DestPath, "/mnt/disk4/movies")
+	assert.EqualValues(t, mockMoveable, filtered[0])
+	assert.EqualValues(t, "/mnt/disk4/movies/dir", filtered[0].DestPath)
+	assert.EqualValues(t, "/mnt/disk4/movies", filtered[0].RootDir.DestPath)
 
 	mockOS.AssertExpectations(t)
 }
@@ -226,9 +226,9 @@ func TestEstablishPaths_Unicode(t *testing.T) {
 
 	assert.NoError(t, err)
 	assert.Len(t, filtered, 1)
-	assert.EqualValues(t, filtered[0], mockMoveable)
-	assert.EqualValues(t, filtered[0].DestPath, "/mnt/disk4/movies/日本国")
-	assert.EqualValues(t, filtered[0].RootDir.DestPath, "/mnt/disk4/movies")
+	assert.EqualValues(t, mockMoveable, filtered[0])
+	assert.EqualValues(t, "/mnt/disk4/movies/日本国", filtered[0].DestPath)
+	assert.EqualValues(t, "/mnt/disk4/movies", filtered[0].RootDir.DestPath)
 
 	mockOS.AssertExpectations(t)
 }
@@ -271,9 +271,9 @@ func TestEstablishPaths_Spaces(t *testing.T) {
 
 	assert.NoError(t, err)
 	assert.Len(t, filtered, 1)
-	assert.EqualValues(t, filtered[0], mockMoveable)
-	assert.EqualValues(t, filtered[0].DestPath, "/mnt/disk4/movi es/日本国")
-	assert.EqualValues(t, filtered[0].RootDir.DestPath, "/mnt/disk4/movi es")
+	assert.EqualValues(t, mockMoveable, filtered[0])
+	assert.EqualValues(t, "/mnt/disk4/movi es/日本国", filtered[0].DestPath)
+	assert.EqualValues(t, "/mnt/disk4/movi es", filtered[0].RootDir.DestPath)
 
 	mockOS.AssertExpectations(t)
 }
