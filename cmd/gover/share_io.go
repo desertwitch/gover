78944d2fae09d0c70511175e89aee275ff00f641
desertwitch <24509509+desertwitch@users.noreply.github.com>
2025-03-15 09:08:26 +0100

    io taskmanager

--- cmd/gover/share_io.go
+++ cmd/gover/share_io.go
@@ -3,39 +3,26 @@ package main
 import (
 	"context"
 	"runtime"
-	"sync"
 
 	"github.com/desertwitch/gover/internal/generic/queue"
 	"github.com/desertwitch/gover/internal/generic/schema"
 )
 
 func ioProcessFiles(ctx context.Context, files []*schema.Moveable, queueMan *queue.Manager, deps *depPackage) error {
-	queueMan.IOManager.Enqueue(files...)
+	tasker := queue.NewTaskManager()
 
+	queueMan.IOManager.Enqueue(files...)
 	queues := queueMan.IOManager.GetQueuesUnsafe()
 
-	var queueWG sync.WaitGroup
-
-	maxWorkers := runtime.NumCPU()
-	semaphore := make(chan struct{}, maxWorkers)
-
 	for _, q := range queues {
-		select {
-		case <-ctx.Done():
-			return ctx.Err()
-		case semaphore <- struct{}{}:
-		}
-
-		queueWG.Add(1)
-		go func(q *queue.IOTargetQueue) {
-			defer queueWG.Done()
-			defer func() { <-semaphore }()
-
+		tasker.Add(func() {
 			deps.IOHandler.ProcessQueue(ctx, q)
-		}(q)
+		})
 	}
 
-	queueWG.Wait()
+	if err := tasker.LaunchConcAndWait(ctx, runtime.NumCPU()); err != nil {
+		return err
+	}
 
 	return nil
 }
