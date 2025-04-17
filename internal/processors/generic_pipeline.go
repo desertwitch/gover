b521c836b2478898fe78dc1b5dc2565c4e917b7c
desertwitch <24509509+desertwitch@users.noreply.github.com>
2025-04-17 09:56:37 +0200

    pipeline docs

--- internal/processors/generic_pipeline.go
+++ internal/processors/generic_pipeline.go
@@ -1,15 +1,27 @@
 package processors
 
-import "github.com/desertwitch/gover/internal/schema"
+import (
+	"github.com/desertwitch/gover/internal/schema"
+)
 
-// GenericPipeline is the principal implementation of a [schema.Pipeline].
+// GenericPipeline is the principal implementation of a [schema.Pipeline]. It
+// holds all [schema.Processor] and [schema.BatchProcessor] functions for a
+// specific operation and provides helper functions to execute them in
+// sequential order and ensuring data safety.
+//
+// During execution, the pipeline exits on the first failed processor. The
+// pipeline itself is not context-aware, rather the passed in [schema.Processor]
+// and [schema.BatchProcessor] can themselves capture a context and handle its
+// cancellation by returning "false" where and when early exit from the overall
+// pipeline is warranted and wanted.
 type GenericPipeline[T any] struct {
 	batchPreProcessors  []schema.BatchProcessor[T]
 	itemProcessors      []schema.Processor[T]
 	batchPostProcessors []schema.BatchProcessor[T]
 }
 
-// Add takes a [schema.Processor] and adds it to the processing pipeline.
+// Add takes a [schema.Processor] and adds it to the processing pipeline for
+// later execution.
 func (p *GenericPipeline[T]) Add(processor schema.Processor[T]) schema.Pipeline[T] {
 	p.itemProcessors = append(p.itemProcessors, processor)
 
@@ -17,7 +29,7 @@ func (p *GenericPipeline[T]) Add(processor schema.Processor[T]) schema.Pipeline[
 }
 
 // AddPreProcess takes a [schema.BatchProcessor] pre-processor and adds it to
-// the pre-processing pipeline.
+// the pre-processing pipeline for later execution.
 func (p *GenericPipeline[T]) AddPreProcess(processor schema.BatchProcessor[T]) schema.Pipeline[T] {
 	p.batchPreProcessors = append(p.batchPreProcessors, processor)
 
@@ -25,14 +37,19 @@ func (p *GenericPipeline[T]) AddPreProcess(processor schema.BatchProcessor[T]) s
 }
 
 // AddPostProcess takes a [schema.BatchProcessor] post-processor and adds it to
-// the post-processing pipeline.
+// the post-processing pipeline for later execution.
 func (p *GenericPipeline[T]) AddPostProcess(processor schema.BatchProcessor[T]) schema.Pipeline[T] {
 	p.batchPostProcessors = append(p.batchPostProcessors, processor)
 
 	return p
 }
 
-// Process sequentially runs all previously added [schema.Processor] processors.
+// Process sequentially runs all previously added [schema.Processor] processors
+// on the given [T]. It is a helper method that is typically only called from
+// within the structure responsible for the given T, such as a queue or
+// queue-related processing function.
+//
+// The first failed processor will cause the function to return with "false".
 func (p *GenericPipeline[T]) Process(item T) bool {
 	for _, fn := range p.itemProcessors {
 		if success := fn(item); !success {
@@ -44,7 +61,13 @@ func (p *GenericPipeline[T]) Process(item T) bool {
 }
 
 // PreProcess sequentially runs all previously added [schema.BatchProcessor]
-// pre-processors.
+// pre-processors. It ensures only copies of the given slice are provided to the
+// respective [schema.BatchProcessor] functions and itself eventually returns a
+// copy of the original slice with all the manipulations. It is a helper method
+// that is typically only called from within the structure responsible for the
+// given slice of [T], such as a queue.
+//
+// The first failed processor will cause the function to return with "false".
 func (p *GenericPipeline[T]) PreProcess(items []T) ([]T, bool) {
 	itemsCopy := make([]T, len(items))
 	copy(itemsCopy, items)
@@ -62,7 +85,13 @@ func (p *GenericPipeline[T]) PreProcess(items []T) ([]T, bool) {
 }
 
 // PostProcess sequentially runs all previously added [schema.BatchProcessor]
-// post-processors.
+// post-processors. It ensures only copies of the given slice are provided to
+// the respective [schema.BatchProcessor] functions and itself eventually
+// returns a copy of the original slice with all the manipulations. It is a
+// helper method that is typically only called from within the structure
+// responsible for the given slice of [T], such as a queue.
+//
+// The first failed processor will cause the function to return with "false".
 func (p *GenericPipeline[T]) PostProcess(items []T) ([]T, bool) {
 	itemsCopy := make([]T, len(items))
 	copy(itemsCopy, items)
--- internal/schema/pipeline.go
+++ internal/schema/pipeline.go
@@ -5,20 +5,38 @@ package schema
 // It should return a success boolean, any output the user needs to be aware of
 // should be made with [Slog] calls, as no error type can be returned through
 // the processor itself.
+//
+// During execution the pipeline will exit on the first failed processor, so
+// "false" should be used only where pipeline failure is intended. The pipeline
+// itself will not be context-aware, rather the Processor can capture a context
+// and handle itself its cancellation by returning "false" where and when early
+// exit from the overall pipeline is warranted and wanted.
 type Processor[T any] func(item T) bool
 
 // BatchProcessor is a function that processes a slice of [T] as part of a
-// [Pipeline]. During execution, only copies of the given slice are operated on,
-// so the processor function need not take care of copying the slice itself.
+// [Pipeline]. During execution, only copies of the original slice are given to
+// the BatchProcessor, so it need not take care of copying the slice itself.
 //
-// It should return a success boolean and the copy of the original given slice
+// It should return a success boolean and a slice (again, it need not be a copy)
 // that is now holding the manipulations after processing. Any output the user
 // needs to be aware of should be made with [Slog] calls, as no error type can
 // be returned through the processor itself.
+//
+// During execution the pipeline will exit on the first failed processor, so
+// "false" should be used only where pipeline failure is intended. The pipeline
+// itself will not be context-aware, rather the BatchProcessor can capture a
+// context and handle itself its cancellation by returning "false" where and
+// when early exit from the overall pipeline is warranted and wanted.
 type BatchProcessor[T any] func(items []T) ([]T, bool)
 
 // Pipeline describes a structure that holds and executes [Processor] processor
 // and [BatchProcessor] pre-/post-processor functions.
+//
+// During execution the pipeline processing functions should return "false" on
+// the first failed processor. The pipeline itself should not be context-aware,
+// rather the passed in [Processor] and [BatchProcessor] can capture a context
+// and handle themselves its cancellation by returning "false" where and when
+// early exit from the overall pipeline is warranted and wanted.
 type Pipeline[T any] interface {
 	// AddPreProcess adds a [BatchProcessor] pre-processor to the pipeline.
 	AddPreProcess(processor BatchProcessor[T]) Pipeline[T]
